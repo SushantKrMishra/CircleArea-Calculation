6a8cec5b801b7ee1eaf2b37505eccd0a1bec1780
SushantKrMishra <76252181+SushantKrMishra@users.noreply.github.com>
2020-12-19 18:36:20 +0530

    Add files via upload

---
+++ abc.py
@@ -0,0 +1,5 @@
+#write a program which accepts the radius of a circle from the user and computes Area
+Radius = float(input("Input the Radius of the Circle: "))
+Area = (22*Radius*Radius)/7
+print("The Area of the Circle with Radius "+str(Radius)+" is: "+str(Area))
+
